d48cfa7c4cbe16495f95e8827f333812bfb53e29
Aidan Hall <aidan.hall34@outlook.com>
2022-12-05 08:49:36 +1100

    First problem

---
+++ day1/solution1.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+import time
+with open('day1/input', 'r') as c:
+    # Get the data from the file 
+    # and split it all into groups
+    groups = c.read().split('\n')
+biggest = 0
+count = 0
+# Keep adding numbers till you hit an empty line
+for num in groups:
+    if num != '':
+        print(f"{num} + {count} = {int(num) + count}")
+        count = int(num) + count
+    else:
+    # Compare the current group to previous largest number
+        if count > biggest:
+            print(f"New largest.\nWas: {biggest}\nIs now:{count}")
+            biggest = count
+        else: print(f"Not larger.\nLargest: {biggest}\nCurrent group: {count}")
+        # Reset to 0 for next group
+        count = 0
+# Print the largest number we found
+print(f"largest number: {biggest}")
