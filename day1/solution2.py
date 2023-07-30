7866e4dc170037588b0cfc7a64428724fefe0d16
Aidan Hall <aidan.hall34@outlook.com>
2023-07-30 10:07:44 +1000

    Backup

--- day1/solution2.py
+++ day1/solution2.py
@@ -3,31 +3,11 @@ import time
 with open('day1/input', 'r') as c:
     # Get the data from the file 
     # and split it all into groups
-    groups = c.read().split('\n')
-biggest = 0
-notwo = 0
-nothree = 0 
-count = 0
-# Keep adding numbers till you hit an empty line
-for num in groups:
-    if num != '':
-        print(f"{num} + {count} = {int(num) + count}")
-        count = int(num) + count
-    else:
-    # Compare the current group to previous largest number
-        if count > biggest:
-            print(f"New largest.\nWas: {biggest}\nIs now:{count}")
-            biggest = count
+    groups = c.readlines()
+for line in groups:
+    if line == '\n':
+        # Split the lines into groups
         
-        else: print(f"Not larger.\nLargest: {biggest}\nCurrent group: {count}")
-        # Reset to 0 for next group
-        count = 0
-# Print the largest number we found
-print(f"largest number: {biggest}")
+        # { {total=, ints=[100, 150, 300]}, {total=, ints=[10,65,200]}}
 
 
-
-
-
-# 165166743
-# 165166743
